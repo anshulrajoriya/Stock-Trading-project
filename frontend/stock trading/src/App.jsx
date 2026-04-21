415d0b44f267432c6622e74a14a4159d8c9211f9
anshul rajoriya <anshulrajoriya007@gmail.com>
2026-04-21 22:10:04 +0530

    starting setup

---
+++ frontend/stock trading/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      {/* <h1 className="bg-red-600"> i am anshul</h1> */}
+          <h1 className="text-5xl text-green-500 text-center mt-10">hii</h1>
+
+    </div>
+  )
+}
+
+export default App
