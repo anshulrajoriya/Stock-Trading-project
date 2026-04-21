c5eb4c9a623936ca925bdf23d798699cd8083b03
anshul rajoriya <anshulrajoriya007@gmail.com>
2026-04-22 00:14:36 +0530

    Structure of components

--- frontend/stock trading/src/App.jsx
+++ frontend/stock trading/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       {/* <h1 className="bg-red-600"> i am anshul</h1> */}
           <h1 className="text-5xl text-green-500 text-center mt-10">hii</h1>
-
+<h2>anshul</h2>
     </div>
   )
 }
